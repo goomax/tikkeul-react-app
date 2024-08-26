01dec7f6a0076d4fdc54e836fc1f66141510af2f
Jiheon Park <harrypark.dev@gmail.com>
2024-08-26 14:14:56 +0900

    feat: 공용 컴포넌트 Image 추가 및 스토리북

---
+++ src/stories/Typography.stories.tsx
@@ -0,0 +1,45 @@
+import Typography from '@/components/common/Typography';
+import { Divider } from '@mui/material';
+
+export default {
+  title: 'Example/Typography',
+  component: Typography,
+  tags: ['autodocs'],
+};
+
+const colors = ['primary', 'secondary', 'dark', 'grey', 'white'] as const;
+const bolds = [true, false] as const;
+
+export const AllTypography: React.FC = () => {
+  return (
+    <div>
+      {colors.map((color) =>
+        bolds.map((bold, index) => (
+          <div
+            key={`${color}-${bold}-${index}`}
+            style={{ marginBottom: '8px', ...(color === 'white' ? { backgroundColor: 'black' } : {}) }}
+          >
+            <Typography fontSize={26} color={color} bold={bold}>
+              {`Typography: color-${color}, bold-${bold}, fontSize-26`}
+            </Typography>
+          </div>
+        )),
+      )}
+      <Divider sx={{ margin: '20px 0' }} />
+
+      {colors.map((color) => (
+        <div
+          key={`${color}`}
+          style={{ marginBottom: '8px', ...(color === 'white' ? { backgroundColor: '#777' } : {}) }}
+        >
+          <Typography color="dark" fontSize={20}>
+            Typography Inline Example:{' '}
+            <Typography inline bold color={color}>
+              color-{color}, bold-true, fontSize-20
+            </Typography>
+          </Typography>
+        </div>
+      ))}
+    </div>
+  );
+};
